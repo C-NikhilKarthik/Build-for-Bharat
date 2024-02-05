7b1ad7f5711cca33240ae33c6686c26252bd0d5d
C-NikhilKarthik <nikhilkarthik241103@gmail.com>
2024-02-06 02:35:30 +0530

    frontend completed

---
+++ client/src/App.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Navbar from "./components/navbar";
+
+export default function App() {
+  return (
+    <div className="w-screen flex p-4 pt-28 lg:p-0 items-center justify-center h-full min-h-screen bg-[url('https://cdn.wallpapersafari.com/20/80/jQPMT3.jpg')] bg-cover bg-center">
+      <Navbar />
+      <div className="max-w-[600px] gap-8 w-full flex-col h-full flex lg:flex-row">
+        <div className="flex flex-col h-full justify-center w-full gap-8">
+          <button className="rounded px-4 py-2 bg-blue-600 text-white font-semibold text-xl">
+            Merchant Delivers
+          </button>
+
+          <button className="rounded px-4 py-2 bg-blue-600/20 border border-blue-500 text-white font-semibold text-xl">
+            Add Merchant
+          </button>
+        </div>
+        <div className="flex w-full flex-col bg-slate-900/20 p-4 border-2 border-blue-500 border-dashed gap-6 rounded">
+          <select className="rounded px-3 py-1">
+            <option value="def">Select Pincode</option>
+          </select>
+
+          <button className="rounded-full px-2 py-1 bg-blue-600 text-white text-sm">
+            Submit
+          </button>
+
+          <div className="text-slate-600">Merchants Available</div>
+          <div className="p-4 rounded bg-slate-600/50 backdrop-blur w-full aspect-square"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ client/src/components/navbar.tsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <div className="p-3 px-6 flex items-center bg-slate-900/50 backdrop-blur justify-between absolute top-0 left-0 w-full">
+      <img
+        className="h-12"
+        src="https://hack2skill.com/brandguidelines/assets/images/H2S_White_Logo.png"
+        alt="logo"
+      />
+      <div className="text-3xl text-blue-500 font-bold">Build For Bharat</div>
+    </div>
+  );
+}
